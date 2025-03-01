7f85ed85ebbe14b1ddac4e422a828a2a9c7b81be
Avijith paul <avijithpaul2006@gmail.com>
2025-03-01 19:30:13 +0500

    function added

---
+++ jsFile/main.js
@@ -0,0 +1,11 @@
+document.getElementById("discover-box").addEventListener('click', function () {
+    window.location.href = "question.html"
+})
+
+clickCompletedBtn("card-1-btn","card-name-1")
+clickCompletedBtn("card-2-btn","card-name-2")
+clickCompletedBtn("card-3-btn","card-name-3")
+clickCompletedBtn("card-4-btn","card-name-4")
+clickCompletedBtn("card-5-btn","card-name-5")
+clickCompletedBtn("card-6-btn","card-name-6")
+
---
+++ jsFile/utilitice.js
@@ -0,0 +1,37 @@
+function getInnerNumber(id) {
+    const innerText = document.getElementById(id).innerText
+    const intoNumber = parseInt(innerText)
+    return intoNumber
+}
+
+// completed btn function 
+function clickCompletedBtn(id,cardName) {
+    document.getElementById(id).addEventListener("click", function () {
+        alert('you done it')
+        document.getElementById(id).style.opacity = '51%'
+
+        const pendingTask = getInnerNumber("pending-task")
+        const convertedPendingTask = pendingTask - 1
+        document.getElementById("pending-task").innerText = convertedPendingTask
+
+        const completedTask = getInnerNumber("completed-task")
+        const convertedCompletedTask = completedTask + 1
+        document.getElementById("completed-task").innerText = convertedCompletedTask
+
+        const cardNam = document.getElementById(cardName).innerText
+        const timeNow = new Date().toLocaleTimeString();
+
+        const p = document.createElement("p")
+        p.classList.add("font-light")
+        p.classList.add("text-xs")
+        p.classList.add("p-2")
+        p.classList.add("bg-slate-100")
+        p.classList.add("rounded-lg")
+        p.innerText = `
+            You have Complete The Task ${cardNam} at ${timeNow}
+        `
+        document.getElementById("history-container").appendChild(p)
+
+        
+    })
+}
