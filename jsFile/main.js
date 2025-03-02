89b69564b22d39d899a6672a39466d3516e69315
Avijith paul <avijithpaul2006@gmail.com>
2025-03-02 21:05:17 +0500

    all done

--- jsFile/main.js
+++ jsFile/main.js
@@ -5,8 +5,8 @@ document.getElementById("discover-box").addEventListener('click', function () {
 
 const date = new Date();
 
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 const day = date.getDate().toString().padStart(2, '0');
 const month = monthsOfYear[date.getMonth()];
@@ -15,15 +15,15 @@ const weekday = daysOfWeek[date.getDay()];
 
 const formatDate = `${day} ${month}, ${year}`;
 
-document.getElementById("calender").innerText = formatDate
-document.getElementById("week").innerText = weekday
+document.getElementById("calender").innerText = formatDate;
+document.getElementById("week").innerText = weekday;
 
-clickCompletedBtn("card-1-btn", "card-name-1")
-clickCompletedBtn("card-2-btn", "card-name-2")
-clickCompletedBtn("card-3-btn", "card-name-3")
-clickCompletedBtn("card-4-btn", "card-name-4")
-clickCompletedBtn("card-5-btn", "card-name-5")
-clickCompletedBtn("card-6-btn", "card-name-6")
+clickCompletedBtn("card-1-btn", "card-name-1");
+clickCompletedBtn("card-2-btn", "card-name-2");
+clickCompletedBtn("card-3-btn", "card-name-3");
+clickCompletedBtn("card-4-btn", "card-name-4");
+clickCompletedBtn("card-5-btn", "card-name-5");
+clickCompletedBtn("card-6-btn", "card-name-6");
 
 
 document.getElementById("history-btn").addEventListener("click", function () {
@@ -31,9 +31,9 @@ document.getElementById("history-btn").addEventListener("click", function () {
 })
 
 
-const colors = ["#EFEBE3", "#F9EAC7", "#95D6A4", "#F2E8FF", "#DEF5FF"]
-let i = 0
+const colors = ["#EFEBE3", "#F9EAC7", "#95D6A4", "#F2E8FF", "#DEF5FF"];
+let i = 0;
 document.getElementById("bg-color").addEventListener("click", function () {
-    document.getElementById("body").style.backgroundColor = colors[i]
-    i = (i + 1) % colors.length
+    document.getElementById("body").style.backgroundColor = colors[i];
+    i = (i + 1) % colors.length;
 })
--- jsFile/utilitice.js
+++ jsFile/utilitice.js
@@ -1,48 +1,44 @@
 function getInnerNumber(id) {
-    const innerText = document.getElementById(id).innerText
-    const intoNumber = parseInt(innerText)
-    return intoNumber
+    const innerText = document.getElementById(id).innerText;
+    const intoNumber = parseInt(innerText);
+    return intoNumber;
 }
 
-let count = 0
+let count = 0;
 
 // completed btn function 
 function clickCompletedBtn(id, cardName) {
     document.getElementById(id).addEventListener("click", function () {
-        alert("Board updated Successfully")
+        alert("Board updated Successfully");
 
-        document.getElementById(id).disabled = true
+        document.getElementById(id).disabled = true;
 
-        const pendingTask = getInnerNumber("pending-task")
-        const convertedPendingTask = pendingTask - 1
-        document.getElementById("pending-task").innerText = convertedPendingTask
+        const pendingTask = getInnerNumber("pending-task");
+        const convertedPendingTask = pendingTask - 1;
+        document.getElementById("pending-task").innerText = convertedPendingTask;
 
-        const completedTask = getInnerNumber("completed-task")
-        const convertedCompletedTask = completedTask + 1
-        document.getElementById("completed-task").innerText = convertedCompletedTask
+        const completedTask = getInnerNumber("completed-task");
+        const convertedCompletedTask = completedTask + 1;
+        document.getElementById("completed-task").innerText = convertedCompletedTask;
 
-        const cardNam = document.getElementById(cardName).innerText
+        const cardNam = document.getElementById(cardName).innerText;
         const timeNow = new Date().toLocaleTimeString();
 
-        const p = document.createElement("p")
-        p.classList.add("font-light")
-        p.classList.add("text-xs")
-        p.classList.add("p-2")
-        p.classList.add("bg-slate-100")
-        p.classList.add("rounded-lg")
+        const p = document.createElement("p");
+        p.classList.add("font-light");
+        p.classList.add("text-xs");
+        p.classList.add("p-2");
+        p.classList.add("bg-slate-100");
+        p.classList.add("rounded-lg");
         p.innerText = `
             You have Complete The Task ${cardNam} at ${timeNow}
         `
-        document.getElementById("history-container").appendChild(p)
+        document.getElementById("history-container").appendChild(p);
 
 
         if (count === 5) {
-            alert("congratulations!!! You have completed all the current task")
+            alert("congratulations!!! You have completed all the current task");
         }
-        count++
+        count++;
     })
-}
-
-function lastAlert(idName) {
-    document.getElementById(idName).disabled === true
 }
